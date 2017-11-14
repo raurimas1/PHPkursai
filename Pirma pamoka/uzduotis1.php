55241fde42827c74f5252e675a8904eb1bbf0f14
Aurimas <aurimas.raisys@gmail.com>
2017-11-14 19:39:42 +0200

    qwe

---
+++ Pirma pamoka/uzduotis1.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <title>Pirmas</title>
+</head>
+<body>
+    <h1>Labas, <?php 
+        $vardas = "Jonas ";
+        echo $vardas; 
+        $a = 10;
+        $z = ++$a + $a++ + $a;
+        echo $z;
+        ?>
+
+        <?php 
+        $x = "kuku";
+        echo $x;
+        ?>
+    </h1>
+    <footer>&copy; <?php echo date("Y"); ?></footer>
+</body>
+</html>
