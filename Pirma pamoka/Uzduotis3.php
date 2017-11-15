1e9d3f7c002ff3250a75c81696b1a3419bc83f90
Aurimas <aurimas.raisys@gmail.com>
2017-11-15 16:00:59 +0200

    nebaigtas (nepradetas)

---
+++ Pirma pamoka/Uzduotis3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $a = array(-10, 0, 2, 9, -5);
+        $i = 0;
+        while ($i < count($a)) {
+            echo "$a[$i], ";
+            $i++;
+        }
+        
+
+    ?>
+</body>
+</html>
