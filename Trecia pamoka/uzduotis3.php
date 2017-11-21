de244bb234f36ad7a297a8577dff8b52aeed2455
Aurimas <aurimas.raisys@gmail.com>
2017-11-21 16:04:12 +0200

    :)

--- Trecia pamoka/uzduotis3.php
+++ Trecia pamoka/uzduotis3.php
@@ -7,45 +7,17 @@ $sum = [];
 foreach ($a as $eilute) {
     foreach ($eilute as $stulpelis => $reiksme) {
         //echo $stulpelis . '-' . $reiksme . ';' . '<br>';
-        $sum[$stulpelis] += $reiksme;
-        
-
-        
+        $sum[$stulpelis] += $reiksme;   
     }
 }
+//echo max($sum);
 
-echo max($sum);
-
-
-
-
-
-
-
-
-
-
-
-// foreach ($sum as $sumos) {
-//     // echo $sumos;
-//     // echo '<br>';
-    
-// }
-// $i = 0;
-// if ($sumos[$i] > $sumos[$i+1]) {
-//     $sumo[$i];
-//     $i++;
-// }    
-// // for ($i=0; $i < count($sum); $i++) { 
-// //     print_r($sum);
-// //     // for ($j=0; $j < count($sum) - 1; $j++) { 
-// //     //     //echo $sum[$j];
-// //     //     if ($sum[$j] < $sum[$j+1]) {
-// //     //         $laikinas = $sum[$j+1];
-// //     //         $sum[$j+1] = $sum[$j];
-// //     //         $sum[$j] = $laikinas;
-// //     //         echo $laikinas;
-            
-
+$x = 0;
+foreach ($sum as $sumos) {
+    if ($sumos > $b) {
+        $x = $sumos;
+    }
+}
+echo $sumos;
 
 ?>
