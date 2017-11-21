daca5a73d3ea14d28fe946eeaad055ba57700476
Aurimas <aurimas.raisys@gmail.com>
2017-11-21 13:44:16 +0200

    :)

--- Trecia pamoka/uzduotis1ir2.php
+++ Trecia pamoka/uzduotis1ir2.php
@@ -4,39 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>7.1 ir 7.2</title>
 </head>
 <body>
     <?php
-    
+
     //du ciklai- isorinis ir vidinis
+    // I uzduotis (Jus viska paraset ant lentos :) )
     $a = ['Jonas', 'Petras', 'Antanas', 'Povilas'];
     for ($i=0; $i < count($a) - 1 ; $i++) { 
-        //echo $a[$i];
         for ($j = $i +1; $j < count($a) ; $j++) { 
-            echo $a[$i] . '-' . $a[j];
+            echo $a[$i] . "-" . $a[$j] . '<br>';
         }
     }
-// if 
 
+echo "<br>";
+echo "<br>";
 
-        // $a = array('Jonas', 'Petras', 'Antanas', 'Povilas');
-        
-        
-        // foreach ($a as $key => $value) {
-        //     for ($i=0; $i < ; $i++) { 
-        //         # code...
-        //     }
-        //      if ($value[$key] != $value[$key+1]) {
-        //         $laikinas = $value .'-'. $value;
-        //         echo $laikinas;
-        //     }
-            
-
-           
-               
-                
-        // }
+    // II uzduotis
+    for ($i=0; $i < count($a); $i++) { 
+        for ($j= 0; $j < count($a); $j++) { 
+            if ($a[$i] != $a[$j]) {
+                Echo $a[$i] . "-" . $a[$j] . '<br>';
+            }
+        }
+    }
     ?>
 </body>
 </html>
