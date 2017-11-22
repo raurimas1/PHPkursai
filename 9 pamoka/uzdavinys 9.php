3998cd87a984b69d4c29d2c010f43457fa26a4c9
Aurimas <aurimas.raisys@gmail.com>
2017-11-22 16:37:14 +0200

    :)

---
+++ 9 pamoka/uzdavinys 9.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>pratimas 9.2</title>
+</head>
+<body>
+<?php
+
+    
+    class Mokinys 
+    {
+        public $vardas;
+        public $pavarde;
+        public $trimestras;
+
+        function __construct($vardas, $pavarde, $trimestras) {
+            $this->vardas = $vardas;
+            $this->pavarde = $pavarde;
+            $this->trimestras = $trimestras;
+            
+
+        }
+        function tvidurikis($trimestras){
+            
+        }
+    }
+    
+    $mokiniai = [
+        new Mokinys("Jonas", "Jonaitis", ['lietuviu' => 8, 'matematika' => 9, 'anglu' => 8]),
+        new Mokinys('Petras', 'Petraitis', ['lietuviu' => 4, 'matematika' => 5, 'anglu' => 2]),
+        new Mokinys('Antanas', 'Antanaitis', ['lietuviu' => 10, 'matematika' => 9, 'anglu' => 8])
+        ];
+    ?>
+    <table border="1">
+        <tr><th>Vardas</th><th>Pavarde</th><th>Trimestro vidurkis</th></tr>    
+            <?php foreach ($mokiniai as $zmogus):?>
+        <tr>
+            <td><?php echo $zmogus->vardas ?></td>
+            <td><?= $zmogus->pavarde ?></td>
+            <td><?= $zmogus->tvidurikis  ?></td>
+        </tr>
+            <?php endforeach; ?>
+    </table>
+    
+</body>
+</html>
