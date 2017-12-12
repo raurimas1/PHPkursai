0848d05ba501bf4ee501d2e84a9132ddce44b11a
Aurimas <aurimas.raisys@gmail.com>
2017-12-12 13:50:22 +0200

    :)

--- 9 pamoka/pratimas 9.1.php
+++ 9 pamoka/pratimas 9.1.php
@@ -11,47 +11,23 @@
         ['vardas' => 'Ona', 'pazymiai' => ['lietuviu' => [10, 9, 10], 'anglu' => [9, 8, 10], 'matematika' => [10, 10, 9, 9]]]
         ];  
         
-        
-        function vidurkis($pazymiai){
-            $sum = 0;
-            foreach ($pazymiai as $pazimys) {
-                $sum += $pazimys;
+        foreach ($mokiniai as $mokinys) {
+            foreach ($mokinys['pazymiai'] as $pazymys) {      
             }
-            return $sum / count($pazymiai);
         }
-
-        // function vidurkiai($mokinys){
-        //     $vidurkis = 0;
-        //     $trimestras = [];
-        //     foreach ($mokinys['pazymiai'] as $dalykas =>$pazymiai) {
-        //         $vidurkis = vidurkis($pazymiai);
-        //         $trimestras[$dalykas] = $vidurkis;
-        //     }
-        //     $mokinys['trimestras'] = $trimestras;
-        // }
-    foreach ($mokiniai as $i => $n) {
-        $mokiniai[$i]['trimestras'] = vidurkiai($n);
-        $mokiniai[$i]['vidurkis'] = vidurkiai($mokiniai[$i]['trimestras']);
-    }
-var_dump($mokiniai);
-
-
-// //rusiavimas
-// for ($i=0; $i < count($mokiniai)-1; $i++) { 
-//     $x = $mokiniai[$i];
-//     for ($j=$i+1; $j < count($mokiniai); $j++) { 
-//         if ($x['vidurkis'] > $mokiniai[$j]['vidurkis']) {
-            
-//         }
-//     }
-// }
-
-
+        
+        function vidurkis($pazymys){
+            $suma = 0;
+            foreach ($pazymys as $ivertinimas) {
+                $suma += $ivertinimas;
+            }
+                return round($suma / count($pazymys));   
+        }
 ?>
 
 <table border="1">
     <tr><th>Vardas</th>
-    <th>pazymiai</th>
+    <th>Lietuviu pazymiai</th>
     <th>Lietuviu vid.</th>
     <th>Anglu pazymiai</th>
     <th>Anglu vid.</th>
@@ -62,22 +38,20 @@ var_dump($mokiniai);
     <?php foreach ($mokiniai as $mokinys): ?>
     <tr>
         <td><?php echo $mokinys['vardas']; ?></td>
-        
         <td><?php 
-            foreach ($mokinys['pazymiai'] as $dalykas => $pazymiai): ?>
-                <div>
-                    <?php echo $dalykas . ':' ; ?>
-                    <?php foreach ($pazymiai as $pazimys): ?>
-                        <?php echo $pazimys . '' ?>
-                        <?php endforeach; ?>
-                </div>
-            <?php endforeach; ?>  
-        ?></td>
-        <td><?php  ?></td>
-        <td><?php  ?></td>
-        <td><?php  ?></td>
-        <td><?php  ?></td>
-        <td><?php  ?></td>
+            foreach ($mokinys['pazymiai']['lietuviu'] as $pazymiai): ?>
+                <?php echo $pazymiai;?>
+            <?php endforeach; ?>  </td>
+        <td><?php echo round(vidurkis($mokinys['pazymiai']['lietuviu'])) ?></td>
+        <td><?php foreach ($mokinys['pazymiai']['anglu'] as $pazymiai): ?>
+                <?php echo $pazymiai;?>
+            <?php endforeach; ?>   </td>
+        <td><?php  echo round(vidurkis($mokinys['pazymiai']['anglu']))?></td>
+        <td><?php  foreach ($mokinys['pazymiai']['matematika'] as $pazymiai): ?>
+                <?php echo $pazymiai;?>
+            <?php endforeach; ?></td>
+        <td><?php  echo round(vidurkis($mokinys['pazymiai']['matematika']))?></td>
+        <td><?php echo $bvid = round((round(vidurkis($mokinys['pazymiai']['lietuviu'])) + round(vidurkis($mokinys['pazymiai']['anglu'])) + round(vidurkis($mokinys['pazymiai']['matematika'])))/count($mokinys['pazymiai'])); ?></td>
     </tr>
     <?php endforeach; ?>
         
